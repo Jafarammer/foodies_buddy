fcf8e75a5d8e8e698e75b2aa49a32a38594ff1cd
jafarammer <jafalkaff96@gmail.com>
2023-12-27 00:12:29 +0700

    create db user (post & get)

---
+++ API/model/user/index.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  phone_number: {
+    type: Number,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  photo_profile: {
+    type: String,
+    required: false,
+  },
+});
+
+module.exports = mongoose.model("User", userSchema);
